2862dafc7b0a1bd3cd1dfcdb879322f46ed78164
Ife-Olaitan <olaiyaifeoluwa.o@gmail.com>
2026-02-06 18:37:09 +0000

    feat: add terraform s3

---
+++ terraform/s3.tf
@@ -0,0 +1,26 @@
+# S3 bucket for marketing assets (product images)
+resource "aws_s3_bucket" "assets" {
+  bucket = "bedrock-assets-${var.student_id}"
+}
+
+# Keep bucket private
+resource "aws_s3_bucket_public_access_block" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Trigger Lambda when a file is uploaded
+resource "aws_s3_bucket_notification" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.asset_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
