94b1f13bd3479c84ffbf32b239871cfaaee9ade0
Ife-Olaitan <olaiyaifeoluwa.o@gmail.com>
2026-02-06 18:38:24 +0000

    feat: add lambda script

---
+++ lambda/index.py
@@ -0,0 +1,6 @@
+def handler(event, context):
+  for record in event['Records']:
+      bucket = record['s3']['bucket']['name']
+      key = record['s3']['object']['key']
+      print(f"Image received: {key}")
+  return {'statusCode': 200}
